6034efca73764d1ca95a09ec560fd0d4b376917e
Francesco Banterle <frabante@gmail.com>
2017-12-20 11:57:02 +0100

    fix

--- install.m
+++ install.m
@@ -1,20 +1,18 @@
 %
-%     HDR Toolbox Installer
+% 
+% Digit
+% An automatic MATLAB app for the digitalization of archaeological drawings. 
+% http://vcg.isti.cnr.it
+% 
+% Copyright (C) 2016-17
+% Visual Computing Laboratory - ISTI CNR
+% http://vcg.isti.cnr.it
+% Main author: Francesco Banterle
+% 
+% This Source Code Form is subject to the terms of the Mozilla Public
+% License, v. 2.0. If a copy of the MPL was not distributed with this
+% file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %
-%     Copyright (C) 2011-2013  Francesco Banterle
-% 
-%     This program is free software: you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation, either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-% 
-%     You should have received a copy of the GNU General Public License
-%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %
 
 disp('Installing the ArchAIDE Digit Tools...');
